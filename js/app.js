aa8f0fc590fa6882ad6d1eeb101b01a22c12e144
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-08-10 00:41:34 -0300

    Mostrando el resto de la información

--- js/app.js
+++ js/app.js
@@ -53,10 +53,20 @@ function carritoHTML() {
 
     // Recoore el carrito y genera el HTML
     articulosCarrito.forEach(curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+
         const row = document.createElement('tr');
         row.innerHTML = `
-            <td>${curso.titulo}</td>
-        `
+            <td>
+                <img src="${imagen}" width="100"/>
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id=${id}>X</a>
+            </td>
+        `;
         // Agregar el HTML del carrito en el tbody
         contenedorCarrito.appendChild(row);
     });
